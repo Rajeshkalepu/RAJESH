d95ab6bab9f2fecbe7cb69b59317d2f13b29ed12
Rajeshkalepu <46395196+Rajeshkalepu@users.noreply.github.com>
2019-01-05 03:01:44 -0800

    Add files via upload

---
+++ Session1.py
@@ -0,0 +1,363 @@
+Python 3.7.1 (v3.7.1:260ec2c36a, Oct 20 2018, 14:05:16) [MSC v.1915 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x=3
+>>> y=7
+>>> x+y
+10
+>>> 
+================ RESTART: C:/Users/IBS/Desktop/Rajesh/sum.py ================
+10
+>>> x=5
+>>> y=5
+>>> x-y
+0
+>>> print(4)
+4
+>>> print("the sum of two numbers:x+y")
+the sum of two numbers:x+y
+>>> print("the sum of two numbers:" x+y)
+SyntaxError: invalid syntax
+>>> print("the sum of two numbers:",sum)
+the sum of two numbers: <built-in function sum>
+>>> sum=x+y
+>>> x+y
+10
+>>> sum = x+y
+>>> type(`Hello, World!`)
+SyntaxError: invalid syntax
+>>> type('Hello,World!')
+<class 'str'>
+>>> type(11)
+<class 'int'>
+>>> type(3.47)
+<class 'float'>
+>>> n=15
+>>> print n
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(n)?
+>>> printn
+Traceback (most recent call last):
+  File "<pyshell#19>", line 1, in <module>
+    printn
+NameError: name 'printn' is not defined
+>>> print(n)
+15
+>>> message = hi
+Traceback (most recent call last):
+  File "<pyshell#21>", line 1, in <module>
+    message = hi
+NameError: name 'hi' is not defined
+>>> message="Hi"
+>>> print ("message")
+message
+>>> minute=30
+>>> minute/60
+0.5
+>>> quotient=7/3
+>>> print quotient
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(quotient)?
+>>> print ("quotient")
+quotient
+>>> quotient=7/3
+>>> printquotient
+Traceback (most recent call last):
+  File "<pyshell#30>", line 1, in <module>
+    printquotient
+NameError: name 'printquotient' is not defined
+>>> print (quotient)
+2.3333333333333335
+>>> remainder =7%3
+>>> print(remainder)
+1
+>>> quotient=in(7/3)
+SyntaxError: invalid syntax
+>>> quotient=int(7/3)
+>>> print(quotient)
+2
+>>> first=10
+>>> second=15
+>>> print(first+second)
+25
+>>> first='100'
+>>> second='150'
+>>> print(first+second)
+100150
+>>> name=input('what is your name?\n')
+what is your name?
+
+>>> name=input('what is your name?\n')
+what is your name?
+Rajes
+>>> print(name)
+Rajes
+>>> prompt=('what is the velocity of car?\n')
+>>> speed=raw_input(prompt)
+Traceback (most recent call last):
+  File "<pyshell#47>", line 1, in <module>
+    speed=raw_input(prompt)
+NameError: name 'raw_input' is not defined
+>>> prompt=('what is the velocity?\n')
+>>> speed=raw_input(prompt)
+Traceback (most recent call last):
+  File "<pyshell#49>", line 1, in <module>
+    speed=raw_input(prompt)
+NameError: name 'raw_input' is not defined
+>>> speed=raw_input('prompt')
+Traceback (most recent call last):
+  File "<pyshell#50>", line 1, in <module>
+    speed=raw_input('prompt')
+NameError: name 'raw_input' is not defined
+>>> int(17)
+17
+>>> 5==5
+True
+>>> 5==2
+False
+>>> 6==int(30/5)
+True
+>>> 6==str('six')
+False
+>>> 2.5==flo(5/2)
+Traceback (most recent call last):
+  File "<pyshell#56>", line 1, in <module>
+    2.5==flo(5/2)
+NameError: name 'flo' is not defined
+>>> type(true)
+Traceback (most recent call last):
+  File "<pyshell#57>", line 1, in <module>
+    type(true)
+NameError: name 'true' is not defined
+>>> type('bool')
+<class 'str'>
+>>> ifx>o:
+	
+SyntaxError: invalid syntax
+>>> 
+================ RESTART: C:/Users/IBS/Desktop/Rajesh/sum.py ================
+x is small
+>>> 
+================ RESTART: C:/Users/IBS/Desktop/Rajesh/sum.py ================
+x is small
+>>> 
+================ RESTART: C:/Users/IBS/Desktop/Rajesh/sum.py ================
+x is small
+x is bigger
+>>> 
+================ RESTART: C:/Users/IBS/Desktop/Rajesh/sum.py ================
+x is small
+x is bigger
+x is even
+>>> 
+================ RESTART: C:/Users/IBS/Desktop/Rajesh/sum.py ================
+x is small
+x is bigger
+x is even
+x is greater than y
+>>> 
+================ RESTART: C:/Users/IBS/Desktop/Rajesh/sum.py ================
+x is small
+x is bigger
+x is even
+x is greater than y
+Traceback (most recent call last):
+  File "C:/Users/IBS/Desktop/Rajesh/sum.py", line 21, in <module>
+    if choice=='a':
+NameError: name 'choice' is not defined
+>>> 
+================ RESTART: C:/Users/IBS/Desktop/Rajesh/sum.py ================
+x is small
+x is bigger
+x is even
+x is greater than y
+Traceback (most recent call last):
+  File "C:/Users/IBS/Desktop/Rajesh/sum.py", line 20, in <module>
+    choice=b
+NameError: name 'b' is not defined
+>>> 
+================ RESTART: C:/Users/IBS/Desktop/Rajesh/sum.py ================
+x is small
+x is bigger
+x is even
+x is greater than y
+input an alphabet
+a
+bad guess
+>>> 
+================ RESTART: C:/Users/IBS/Desktop/Rajesh/sum.py ================
+x is small
+x is bigger
+x is even
+x is greater than y
+input an alphabet
+c
+close,but not correct
+>>> 
+================ RESTART: C:/Users/IBS/Desktop/Rajesh/sum.py ================
+x is small
+x is bigger
+x is even
+x is greater than y
+input an alphabet
+35
+Enter Fahrenheit Temperature72
+22.22222222222222
+>>> 
+================ RESTART: C:/Users/IBS/Desktop/Rajesh/sum.py ================
+x is small
+x is bigger
+x is even
+x is greater than y
+input an alphabet
+35
+Enter Fahrenheit Temperature80
+26.666666666666668
+>>> 
+>>> 
+================ RESTART: C:/Users/IBS/Desktop/Rajesh/sum.py ================
+x is small
+x is bigger
+x is even
+x is greater than y
+input an alphabet
+
+Enter Fahrenheit Temperature35
+1.6666666666666667
+>>> 
+================ RESTART: C:/Users/IBS/Desktop/Rajesh/sum.py ================
+x is small
+x is bigger
+x is even
+x is greater than y
+input an alphabet
+x is bigger
+Enter Fahrenheit Temperature5
+-15.0
+Enter Fahrenheit Temperature:a
+print enter a number
+>>> 56
+56
+>>> 
+>>> 
+>>> 
+================ RESTART: C:/Users/IBS/Desktop/Rajesh/sum.py ================
+x is small
+x is bigger
+x is even
+x is greater than y
+input an alphabet
+
+Enter Fahrenheit Temperature72
+22.22222222222222
+Enter Fahrenheit Temperature:s
+please enter a number
+>>> 
+================ RESTART: C:/Users/IBS/Desktop/Rajesh/sum.py ================
+x is small
+x is bigger
+x is even
+x is greater than y
+input an alphabet
+
+Enter Fahrenheit Temperature
+================ RESTART: C:/Users/IBS/Desktop/Rajesh/sum.py ================
+x is small
+x is bigger
+x is even
+x is greater than y
+input an alphabet
+
+Enter Fahrenheit Temperature
+Traceback (most recent call last):
+  File "C:/Users/IBS/Desktop/Rajesh/sum.py", line 28, in <module>
+    fahr=float(inp)
+ValueError: could not convert string to float: 
+>>> 
+================ RESTART: C:/Users/IBS/Desktop/Rajesh/sum.py ================
+x is small
+x is bigger
+x is even
+x is greater than y
+input an alphabet
+
+Enter Fahrenheit Temperature45
+7.222222222222222
+Enter Fahrenheit Temperature:j3
+please enter a number
+Enter Hours:70
+Enter Rate:40
+pay
+>>> 
+>>> 
+================ RESTART: C:/Users/IBS/Desktop/Rajesh/sum.py ================
+x is small
+x is bigger
+x is even
+x is greater than y
+input an alphabet
+
+Enter Fahrenheit Temperature50
+10.0
+Enter Fahrenheit Temperature:50
+10.0
+Enter Hours:40
+Enter Rate:10
+400
+>>> 
+================ RESTART: C:/Users/IBS/Desktop/Rajesh/sum.py ================
+x is small
+x is bigger
+x is even
+x is greater than y
+input an alphabet
+
+Enter Fahrenheit Temperature
+Traceback (most recent call last):
+  File "C:/Users/IBS/Desktop/Rajesh/sum.py", line 28, in <module>
+    fahr=float(inp)
+ValueError: could not convert string to float: 
+>>> 
+================ RESTART: C:/Users/IBS/Desktop/Rajesh/sum.py ================
+x is small
+x is bigger
+x is even
+x is greater than y
+input an alphabet
+
+Enter Fahrenheit Temperature40
+4.444444444444445
+Enter Fahrenheit Temperature:40
+4.444444444444445
+Enter Hours:50
+Enter Rate:10
+750.0
+>>> 5
+5
+>>> 
+================ RESTART: C:/Users/IBS/Desktop/Rajesh/sum.py ================
+x is small
+x is bigger
+x is even
+x is greater than y
+input an alphabet
+
+Enter Fahrenheit Temperature40
+4.444444444444445
+Enter Fahrenheit Temperature:10
+-12.222222222222221
+Enter Hours:
+==================== RESTART: C:/Users/IBS/Desktop/PAY.py ====================
+Enter Hours:40
+Enter Rate:10
+400
+>>> 
+==================== RESTART: C:/Users/IBS/Desktop/PAY.py ====================
+Enter Hours:gd
+Traceback (most recent call last):
+  File "C:/Users/IBS/Desktop/PAY.py", line 1, in <module>
+    input_hours=int(input('Enter Hours:'))
+ValueError: invalid literal for int() with base 10: 'gd'
+>>> 
+==================== RESTART: C:/Users/IBS/Desktop/PAY.py ====================
+Enter Hours:e
+Enter Rate:t
+Please enter a number
+>>> 
